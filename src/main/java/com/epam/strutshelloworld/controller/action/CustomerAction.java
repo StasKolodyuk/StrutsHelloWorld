ab0d8306731b6f89a69612028143f6069d0dcf25
Stas <staskolodyuk@gmail.com>
2015-02-20 13:59:17 +0300

    Added Form and Action

--- src/main/java/com/epam/strutshelloworld/controller/action/CustomerAction.java
+++ src/main/java/com/epam/strutshelloworld/controller/action/CustomerAction.java
@@ -1,9 +1,31 @@
 
 package com.epam.strutshelloworld.controller.action;
 
+import com.epam.strutshelloworld.controller.form.CustomerForm;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
 
-public class CustomerAction {
-    
-    
+
+public class CustomerAction extends Action {
+
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, 
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
+        
+        if(isCancelled(request)) {
+            System.out.println("Operation cancelled");
+            return mapping.findForward("mainpage");
+        }
+        
+        CustomerForm customerForm = (CustomerForm)form;
+        String firstName = customerForm.getFirstName();
+        String lastName = customerForm.getLastName();
+        System.out.println(firstName + " " + lastName);
+        return mapping.findForward("success");
+    }
     
 }
--- src/main/java/com/epam/strutshelloworld/controller/form/CustomerForm.java
+++ src/main/java/com/epam/strutshelloworld/controller/form/CustomerForm.java
@@ -1,7 +1,38 @@
 
 package com.epam.strutshelloworld.controller.form;
 
+import org.apache.struts.action.ActionForm;
 
-public class CustomerForm {
+
+public class CustomerForm extends ActionForm {
+    
+    private String firstName;
+    private String lastName;
+
+    public CustomerForm() {
+        this.firstName = "";
+        this.lastName = "";
+    }
+
+    public CustomerForm(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
     
 }
